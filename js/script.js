0a4c5495ec71f2b91fed34fea9250718b4df5822
Natalia Marcinowska <marcinowska.natalia@gmail.com>
2022-02-07 17:15:35 +0100

    added onFormSubmit

--- js/script.js
+++ js/script.js
@@ -22,11 +22,28 @@
 
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        };
+        
+        newTaskElement.focus();
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
 
-
 }
+
