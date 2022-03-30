a6febe2b8f3d335a1a081f62239c67c111b13bde
Natalia Marcinowska <marcinowska.natalia@gmail.com>
2022-03-30 21:17:18 +0200

    syntax corrected

--- js/script.js
+++ js/script.js
@@ -62,17 +62,14 @@
 
     const renderTasks = () => {
         const taskToHTML = (task) => `
-        <li class="
-            tasks__item${task.done && hideDoneTasks ? "tasks__item--hidden" : ""} js-tasks
-        ">
-        <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-            ${task.done ? "✔" : ""}
-        </button>
-        <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
-            ${task.content}
-        </span>
-        <button class="tasks__button tasks__button--remove js-remove">
-        </button>
+        <li class="tasks__item${task.done && hideDoneTasks ? "tasks__item--hidden" : ""} js-tasks">
+            <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+                ${task.done ? "✔" : ""}
+            </button>
+            <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
+                ${task.content}
+            </span>
+            <button class="tasks__button tasks__button--remove js-remove"></button>
         </li>
         `;
 
@@ -90,12 +87,10 @@
 
         buttonsElement.innerHTML = `
         <button class="buttons__button js-toggleHideDoneTasks">
-        ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+            ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
         </button>
-        <button class="buttons__button js-markAllDone"
-        ${tasks.every(({done}) => done) ? "disabled" : ""}
-        >
-        Ukończ wszystkie
+        <button class="buttons__button js-markAllDone"${tasks.every(({ done }) => done) ? "disabled" : ""}>
+            Ukończ wszystkie
         </button>
         `;
     };
